7840ba2e4a7a328bc094c8f7ab021dd6ca37baea
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-03-04 19:59:57 +0100

    Add tests

---
+++ test/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/*
+Setup testing environment
+=========================
+*/
+//Testing modules
+var test = require('tape');
+var colorize = require('tap-colorize');
+var colorizeOptions = {
+	pass : '#B2D9B5',
+	fail : '#FE5A4E',
+	info : '#EEEEEE'
+};
+test.createStream()
+	.pipe(colorize(colorizeOptions))
+	.pipe(process.stdout);
+
+//Module to test
+var path = require('../index.js');
+
+/*
+Tests
+=====
+*/
