396cb6065d475361f394d68ce9e75cc9f800b558
Daniel Cobo <danielcobo@users.noreply.github.com>
2017-03-05 18:05:12 +0100

    Add index.js to merge everything into one module

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//Helper function
+var avaliableProp = function(obj, prop){
+	if (typeof obj[prop] !== 'undefined'){
+		throw new Error ('Namespace conflict: ' + prop);
+	}
+}
+
+var path = require('path');
+
+avaliableProp(path, 'lvls');
+path.lvls = require('./lvls.js');
+
+avaliableProp(path, 'byDepth');
+path.byDepth = require('./byDepth.js');
+
+avaliableProp(path, 'ext');
+path.ext = require('./ext.js');
+
+avaliableProp(path, 'rmExt');
+path.rmExt = require('./rmExt.js');
+
+module.exports = path;
